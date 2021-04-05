3f724b14e832b3abda0c6c3eb02866bb6b4e165a
William Araujo <williamf1f2@gmail.com>
2021-04-05 18:52:42 -0300

    Cria components para Header, Footer e Login

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import './App.css'
+import Footer from './components/Footer/Footer'
+import Header from './components/Header/Header'
+import Home from './components/Home/Home'
+import Login from './components/Login/Login'
 
-
+// Routes é o novo Switch
 const App = () => {
   return (
-    <div>Projeto Dogs - Will</div>
+    <div>
+      <Router>
+        <Header />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route />
+        </Routes>
+        <Footer />
+      </Router>
+    </div>
   )
 }
 
